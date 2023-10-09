a1799edeb1dc1a650fb115319296e071e2e4c021
bhavyaNagarro <122007430+bhavyaNagarro@users.noreply.github.com>
2023-10-09 10:35:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const PassBtn = document.querySelector('#passBtn');
+PassBtn.addEventListener('click', () => {
+    const input = document.querySelector('#passInput');
+    input.getAttribute('type') === 'password' ? input.setAttribute('type', 'text') : input.setAttribute('type', 'password');
+
+   if (input.getAttribute('type') === 'text'){
+     PassBtn.innerHTML = '<i class="fa fa-eye-slash"></i>';
+  } else{
+     PassBtn.innerHTML = '<i class="fa fa-eye fa-fw" aria-hidden="true"></i>';
+  }
+
+});
